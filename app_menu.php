9cb003cdd19ad5592ec90f728826133f218f8b9d
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-08-30 16:12:01 -0600

    Update app_menu.php (#8)

--- app_menu.php
+++ app_menu.php
@@ -1,19 +1,34 @@
 <?php
-
-$apps[$x]['menu'][0]['title']['en-us'] = 'Providers';
-$apps[$x]['menu'][0]['title']['es-cl'] = '';
-$apps[$x]['menu'][0]['title']['fr-fr'] = '';
-$apps[$x]['menu'][0]['title']['fr-ca'] = '';
-$apps[$x]['menu'][0]['title']['pl'] = '';
-$apps[$x]['menu'][0]['title']['sv-se'] = '';
-$apps[$x]['menu'][0]['title']['uk'] = '';
-$apps[$x]['menu'][0]['title']['de-at'] = '';
-$apps[$x]['menu'][0]['uuid'] = '76c4e42e-5118-42a7-b7af-0cfc5d41f6e8';
-$apps[$x]['menu'][0]['parent_uuid'] = 'bc96d773-ee57-0cdd-c3ac-2d91aba61b55';
-$apps[$x]['menu'][0]['category'] = 'internal';
-$apps[$x]['menu'][0]['path'] = '/app/providers/providers.php';
-$apps[$x]['menu'][0]['groups'][] = 'superadmin';
-//$apps[$x]['menu'][0]['groups'][] = 'admin';
-//$apps[$x]['menu'][0]['groups'][] = 'user';
+  
+  $apps[$x]['menu'][0]['title']['en-us'] = 'Providers';
+  $apps[$x]['menu'][0]['title']['en-gb'] = "Providers";
+  $apps[$x]['menu'][0]['title']['ar-eg'] = "مقدمي الخدمات";
+  $apps[$x]['menu'][0]['title']['de-at'] = "Anbieter";
+  $apps[$x]['menu'][0]['title']['de-ch'] = "Anbieter";
+  $apps[$x]['menu'][0]['title']['de-de'] = "Anbieter";
+  $apps[$x]['menu'][0]['title']['es-cl'] = "Proveedores";
+  $apps[$x]['menu'][0]['title']['es-mx'] = "Proveedores";
+  $apps[$x]['menu'][0]['title']['fr-ca'] = "Fournisseurs";
+  $apps[$x]['menu'][0]['title']['fr-fr'] = "Fournisseurs";
+  $apps[$x]['menu'][0]['title']['he-il'] = "ספקים";
+  $apps[$x]['menu'][0]['title']['it-it'] = "Fornitori";
+  $apps[$x]['menu'][0]['title']['nl-nl'] = "Aanbieders";
+  $apps[$x]['menu'][0]['title']['pl-pl'] = "Dostawcy";
+  $apps[$x]['menu'][0]['title']['pt-br'] = "Provedores";
+  $apps[$x]['menu'][0]['title']['pt-pt'] = "Provedores";
+  $apps[$x]['menu'][0]['title']['ro-ro'] = "Furnizorii";
+  $apps[$x]['menu'][0]['title']['ru-ru'] = "Провайдеры";
+  $apps[$x]['menu'][0]['title']['sv-se'] = "Leverantörer";
+  $apps[$x]['menu'][0]['title']['uk-ua'] = "Провайдери";
+  $apps[$x]['menu'][0]['title']['zh-cn'] = "供应商";
+  $apps[$x]['menu'][0]['title']['ja-jp'] = "プロバイダー";
+  $apps[$x]['menu'][0]['title']['ko-kr'] = "공급자";
+  $apps[$x]['menu'][0]['uuid'] = '76c4e42e-5118-42a7-b7af-0cfc5d41f6e8';
+  $apps[$x]['menu'][0]['parent_uuid'] = 'bc96d773-ee57-0cdd-c3ac-2d91aba61b55';
+  $apps[$x]['menu'][0]['category'] = 'internal';
+  $apps[$x]['menu'][0]['path'] = '/app/providers/providers.php';
+  $apps[$x]['menu'][0]['groups'][] = 'superadmin';
+  //$apps[$x]['menu'][0]['groups'][] = 'admin';
+  //$apps[$x]['menu'][0]['groups'][] = 'user';
 
 ?>
